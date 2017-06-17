5c9be10d4ec847856ddbf2d058b9153f2a52cb3d
gzapodea <gzapodea@cisco.com>
2017-06-16 17:31:37 -0700

    work in progress, tested with CSR 1000v 16.5.1a

--- get_netconf_info.py
+++ get_netconf_info.py
@@ -11,6 +11,7 @@
 from ncclient import manager
 import sys
 import xml.dom.minidom
+import json
 
 
 
@@ -28,9 +29,20 @@ USER = 'cisco'
 PASS = 'cisco'
 
 
+def pprint(json_data):
+    """
+    Pretty print JSON formatted data
+    :param json_data:
+    :return:
+    """
+
+    print(json.dumps(json_data, indent=4, separators=(' , ', ' : ')))
+
+
 def get_hostname():
     """
     This function will retrieve the hostname from config via NETCONF.
+    :return hostname: device hostname
     """
 
     with manager.connect(host=HOST, port=PORT, username=USER,
@@ -50,56 +62,14 @@ def get_hostname():
         result = m.get_config('running', hostname_filter)
         xml_doc = xml.dom.minidom.parseString(result.xml)
         hostname = xml_doc.getElementsByTagName('hostname')
-        return hostname[0].firstChild.nodeValue
-
-
-def get_interfaces():
-    """
-    This function will return the interfaces info via NETCONF.
-    """
-    with manager.connect(host=HOST, port=PORT, username=USER,
-                         password=PASS, hostkey_verify=False,
-                         device_params={'name': 'default'},
-                         allow_agent=False, look_for_keys=False) as m:
-        # XML filter to issue with the get operation
-        # IOS-XE 16.5+        YANG model called http://cisco.com/ns/yang/Cisco-IOS-XE-native
-
-        interface_filter = '''
-                            <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-                                <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces"/>
-                                <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
-                                    <license>
-                                        <udi>
-                                            <sn/>
-                                        </udi>
-                                    </license>
-                                </native>
-                            </filter>
-                            '''
-
-        result = m.get_config('running', interface_filter)
-        xml_doc = xml.dom.minidom.parseString(result.xml)
-        interfaces = []
-        interface_name = xml_doc.getElementsByTagName('name')
-        number_int = len(interface_name)
-        index = 0
-        while index < number_int:
-            interfaces.append(interface_name[index].firstChild.nodeValue)
-            index += 1
-        return interfaces
-
-
-def get_interface_state(interface_list):
-    """
-
-    :param interface_list:
-    :return:
-    """
+    return hostname[0].firstChild.nodeValue
 
 
 def get_sn():
     """
     This function will return the S/N of the network device using NETCONF
+    :return serial_number - device serial number
+
     """
 
     with manager.connect(host=HOST, port=PORT, username=USER,
@@ -124,7 +94,112 @@ def get_sn():
         result = m.get_config('running', sn_filter)
         xml_doc = xml.dom.minidom.parseString(result.xml)
         serial_number = xml_doc.getElementsByTagName('sn')
-        return serial_number[0].firstChild.nodeValue
+    return serial_number[0].firstChild.nodeValue
+
+
+def get_interfaces():
+    """
+    This function will return the interfaces info via NETCONF.
+    :return interfaces: list of device interfaces
+    """
+
+    with manager.connect(host=HOST, port=PORT, username=USER,
+                         password=PASS, hostkey_verify=False,
+                         device_params={'name': 'default'},
+                         allow_agent=False, look_for_keys=False) as m:
+        # XML filter to issue with the get operation
+        # IOS-XE 16.5+        YANG model called yang:http://cisco.com/ns/yang/Cisco-IOS-XE-native
+
+        interface_filter = '''
+                            <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                                <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces"/>
+                                <native xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-native">
+                                    <license>
+                                        <udi>
+                                            <sn/>
+                                        </udi>
+                                    </license>
+                                </native>
+                            </filter>
+                            '''
+
+        result = m.get_config('running', interface_filter)
+        xml_doc = xml.dom.minidom.parseString(result.xml)
+        interfaces = []
+        interface_name = xml_doc.getElementsByTagName('name')
+        number_int = len(interface_name)
+        index = 0
+        while index < number_int:
+            interfaces.append(interface_name[index].firstChild.nodeValue)
+            index += 1
+    return interfaces
+
+
+def get_interface_state(interface):
+    """
+    This function will get the interface state for the specified interface
+    :param interface: interface name
+    :return interface admin and operational state
+    """
+
+    with manager.connect(host=HOST, port=PORT, username=USER,
+                         password=PASS, hostkey_verify=False,
+                         device_params={'name': 'default'},
+                         allow_agent=False, look_for_keys=False) as m:
+        # XML filter to issue with the get operation
+        # IOS-XE 16.5+        YANG model called yang:ietf-interfaces
+
+        interface_state_filter = '''
+                                     <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                                          <interfaces-state xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+                                                <interface>
+                                                        <name>''' + interface + '''</name>
+                                                </interface>
+                                          </interfaces-state>
+                                     </filter>
+                                '''
+
+        result = m.get(interface_state_filter)
+        xml_doc = xml.dom.minidom.parseString(result.xml)
+        admin_state = xml_doc.getElementsByTagName('admin-status')
+        int_admin_state = admin_state[0].firstChild.nodeValue
+        oper_state = xml_doc.getElementsByTagName('oper-status')
+        int_oper_state = oper_state[0].firstChild.nodeValue
+    return int_admin_state, int_oper_state
+
+
+def get_interface_ip(interface):
+    """
+    This function will retrieve the IPv4 address configured on the interface
+    :param interface: interface name
+    :return: int_ip_add: the interface IPv4 address
+    """
+
+    with manager.connect(host=HOST, port=PORT, username=USER,
+                         password=PASS, hostkey_verify=False,
+                         device_params={'name': 'default'},
+                         allow_agent=False, look_for_keys=False) as m:
+        # XML filter to issue with the get operation
+        # IOS-XE 16.5+        YANG model called yang:ietf-interfaces
+
+        interface_state_filter = '''
+                                    <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                                        <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+                                            <interface>
+                                                <name>''' + interface + '''</name>
+                                            </interface>
+                                        </interfaces>
+                                        </filter>
+                                '''
+        result = m.get(interface_state_filter)
+        xml_doc = xml.dom.minidom.parseString(result.xml)
+        ip_add = xml_doc.getElementsByTagName('ip')
+        try:
+            int_ip_add = ip_add[0].firstChild.nodeValue
+        except:
+            int_ip_add = 'not configured'
+
+        return int_ip_add
 
 
 def main():
@@ -151,10 +226,26 @@ def main():
     print('\nThe network device S/N is:', device_sn)
 
     # get the device interfaces
+
     interfaces_list = get_interfaces()
     print('\nThe network device has these interfaces:')
     for intf in interfaces_list:
         print('    ', intf)
+
+    # get the admin and operational state for each interface
+
+    interface_info=[]
+    for intf in interfaces_list:
+        admin_state = get_interface_state(intf)[0]
+        oper_state = get_interface_state(intf)[1]
+        ip_address = get_interface_ip(intf)
+        interface_info.append({'interface': intf, 'ip address': ip_address, 'admin': admin_state, 'protocol': oper_state})
+
+    # print interface info
+
+    print('\nThe device interfaces info:\n')
+    pprint(interface_info)
+
 
     print('\n\nEnd of application run')
 
